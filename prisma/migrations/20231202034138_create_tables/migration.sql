58ccf3cbd9b6cd09ae0becfbd17defd9ed5741bf
Barbosa885 <gustavosports7@gmail.com>
2023-12-02 04:47:04 -0300

    :recycle: refactor model

--- prisma/migrations/20231202034138_create_tables/migration.sql
+++ prisma/migrations/20231202034138_create_tables/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "Task" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
+    "description" TEXT,
     "completed" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
