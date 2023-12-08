3d31e162eedd3250feb810485e8f339b22838131
Barbosa885 <gustavosports7@gmail.com>
2023-12-08 13:38:27 -0300

    :truck: renames tags to tag

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
-import { TagsModule } from './tags/tags.module';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [TaskModule, TagsModule],
+  imports: [TaskModule, TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
