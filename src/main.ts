499b521b79dd54d5a7bfa70d46b144f402314d48
Barbosa885 <gustavosports7@gmail.com>
2023-12-08 13:23:27 -0300

    :safety_vest: adds class validations to DTO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // isso vai remover qualquer propriedade que não esteja no DTO
+    forbidNonWhitelisted: true, // isso vai retornar um erro caso uma propriedade não esteja no DTO
+    transform: true, // isso vai transformar os dados para o tipo especificado no DTO
+  })
+  )
   await app.listen(3000);
 }
 bootstrap();
--- src/task/dto/create-task.dto.ts
+++ src/task/dto/create-task.dto.ts
@@ -1,5 +1,18 @@
+import { IsString, IsNotEmpty, IsOptional, IsBoolean } from "class-validator";
+
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsNotEmpty()
+  @IsBoolean()
   completed: boolean;
+
+  @IsString({ each: true })
+  tags: string[]
 }
