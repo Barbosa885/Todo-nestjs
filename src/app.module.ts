4bd7d5777911cd4f22ea669fffbb66a2d2a76dbd
Barbosa885 <gustavosports7@gmail.com>
2023-12-08 13:28:04 -0300

    :fire: removes category resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
-import { CategoryModule } from './category/category.module';
-import { CategoryController } from './category/category.controller';
 
 @Module({
-  imports: [TaskModule, CategoryModule],
+  imports: [TaskModule],
   controllers: [AppController],
   providers: [AppService],
 })
