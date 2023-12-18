0d3222b12c1b7cb0b421145b8050561c59152fcc
Barbosa885 <gustavosports7@gmail.com>
2023-12-18 18:38:56 -0300

    :children_crossing: return the tag name at response

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { PrismaService } from 'src/prisma.service';
@@ -8,19 +8,48 @@ export class TaskService {
   constructor(private prisma: PrismaService) {}
 
   async create(createTaskDto: CreateTaskDto)  {
+    const { title, description, completed, tagId } = createTaskDto
+
+    const tag = await this.prisma.tags.findUnique({
+      where: { id: tagId }
+    })
+
+    if (!tag) {
+      throw new BadRequestException(`Categoria não existe`)
+    }
+
+    const tagName = tag.name;
+    console.log(`Nome da categoria associada à tarefa: ${tagName}`);
+
     return await this.prisma.task.create({
-      data: createTaskDto
+      data: {
+        title,
+        description,
+        completed,
+        tag: {
+          connect: {
+            id: tagId
+          }
+        }
+      }
     })
   }
 
   async findAll() {
-    return await this.prisma.task.findMany()
+    return await this.prisma.task.findMany({
+      include: {
+        tag: true
+      }
+    })
   }
 
   async findOne(id: number) {
     return await this.prisma.task.findUnique({
       where: {
         id: id
+      },
+      include: {
+        tag: true
       }
     })
   }
