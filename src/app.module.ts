6702fc7bb5eff08ce3544ab98d162d065f2691b1
Barbosa885 <gustavosports7@gmail.com>
2023-12-18 10:38:05 -0300

    :adhesive_bandage: fix app module imports errors

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TaskModule } from './task/task.module';
 import { TagModule } from './tag/tag.module';
 
 @Module({
   imports: [TaskModule, TagModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 
 export class AppModule {}
